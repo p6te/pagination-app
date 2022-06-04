617bfe155ac017f8f36672f3f5a6a85672b700ec
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-04 09:33:40 +0200

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Images from "./components/images";
+import "./App.scss";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  const URL = "https://jsonplaceholder.typicode.com/albums/1/photos";
+  useEffect(() => {
+    fetch(URL)
+      .then((resp) => resp.json())
+      .then((data) => setImages(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Images data={images} />
     </div>
   );
 }
